9550a5d2ccc593fefa74c912e78e05b69dd2c79f
Jason <jie38863558@gmail.com>
2024-11-06 22:14:18 +0800

    rm wrong file

--- batch/order.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export PGHOST=prd-db.cb04ggmik654.ap-northeast-1.rds.amazonaws.com
-export PGDATABASE=postgres
-export PGUSER=root
-export PGPASSWORD=aA9527123
-psql -c "\COPY order_main TO '/tmp/order.csv' WITH CSV DELIMITER ',';"
-exit 0
--- batch/order_s3.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/local/bin/aws s3 mv /tmp/order.csv s3://wbload1015s3/prd-data-transform-bucket/order_`date '+%Y%m%d%H%M%S'`.csv
-exit 0
